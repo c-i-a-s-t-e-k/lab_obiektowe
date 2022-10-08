919c0ea209827abe06930acd2fcd3a825cd62438
c-i-a-s-t-e-k <jezowdominik@gmail.com>
2022-10-08 16:13:34 +0100

    adding enum Direction to the program

---
+++ src/main/java/agh/ics/oop/Direction.java
@@ -0,0 +1,9 @@
+package agh.ics.oop;
+
+enum Direction {
+    Forward,
+    Backward,
+    Right,
+    Left,
+    Nothing
+}
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,27 +2,32 @@ package agh.ics.oop;
 //https://github.com/apohllo/obiektowe-lab/tree/master/lab1
 public class World {
     public static void main(String[] args){
-        System.out.println("system wystartowal");
-        run(args);
+        System.out.println("system wystartowal\n");
+        Direction[] enumArgs = new Direction[args.length];
+        for(int i = 0;i < args.length; i++) {
+            enumArgs[i] = switch (args[i]){
+                case "f" -> Direction.Forward;
+                case "b" -> Direction.Backward;
+                case "l" -> Direction.Left;
+                case "r" -> Direction.Right;
+                default -> Direction.Nothing;
+            };
+        }
+        run(enumArgs);
         System.out.println("system zakonczyl dzialanie");
     }
 
-    public static void run(String[] args){
-        for(String element : args){
-            System.out.print(element);
-            System.out.print(",");
-            if (element.equals("f")){
-                System.out.println("zwierzak idzie do przodu");
-            }
-            if (element.equals("b")){
-                System.out.println("zwierzak idzie do tylu");
-            }
-            if (element.equals("l")){
-                System.out.println("zwierzak idzie w lewo");
-            }
-            if (element.equals("r")){
-                System.out.println("zwierzak idzie w prawo");
-            }
+    public static void run(Direction[] args){
+        String message;
+        for(Direction element : args){
+            message = switch (element){
+                case Left -> "pojazd skręcił w lewo";
+                case Right -> "pojazd skręcił w prawo";
+                case Backward -> "pojazd jedzie do tyłu";
+                case Forward -> "pojazd jedzie do przodu";
+                default -> "nieznany kierunek";
+            };
+            System.out.println(message);
         }
         System.out.println();
     }
