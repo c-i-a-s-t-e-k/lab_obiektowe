70082abc5cdd8658225c8de6a97d228564a2f0fa
c-i-a-s-t-e-k <jezowdominik@gmail.com>
2022-10-16 16:17:09 +0100

    adding Vector2d class

--- src/main/java/agh/ics/oop/Vector2d.java
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -1,4 +1,5 @@
 package agh.ics.oop;
+import java.lang.String;
 
 public class Vector2d {
     public final int x;
@@ -9,6 +10,41 @@ public class Vector2d {
         this.y = y;
     }
 
-//    public String toString(){
-//    }
+    public String toString(){
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+    public boolean precedens(Vector2d other){
+        return (this.x <= other.x) && (this.y <= other.y);
+     }
+
+     public boolean follows(Vector2d other){
+        return (this.x >= other.x) && (this.y >= other.y);
+     }
+
+     public Vector2d add(Vector2d other){
+        return new Vector2d(this.x + other.x, this.y + other.y);
+     }
+
+    public Vector2d subtract(Vector2d other){
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+    public Vector2d upperRight(Vector2d other){
+        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
+    }
+
+    public Vector2d lowerLeft(Vector2d other){
+        return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
+    }
+
+    public Vector2d opposite(){
+        return new Vector2d(-this.x, -this.y);
+    }
+
+    public boolean equals(Object other){
+        return (this.x == ((Vector2d) other).x) && (this.y == ((Vector2d) other).y);
+    }
+
+
 }
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,10 +3,16 @@ import java.util.Arrays;
 //https://github.com/apohllo/obiektowe-lab/tree/master/lab1
 public class World {
     public static void main(String[] args) {
-        System.out.println("system wystartowal\n");
-        Direction[] enumArgs = stringsToEnums(args);
-        run(enumArgs);
-        System.out.println("system zakonczyl dzialanie");
+//        System.out.println("system wystartowal\n");
+//        Direction[] enumArgs = stringsToEnums(args);
+//        run(enumArgs);
+//        System.out.println("system zakonczyl dzialanie");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     public static void run(Direction[] args) {
