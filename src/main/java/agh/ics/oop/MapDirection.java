5cf2d15d7b40748b8cc651baa06cc94a66dd3aae
c-i-a-s-t-e-k <jezowdominik@gmail.com>
2022-10-18 10:31:35 +0100

    optymalizacja MapDirection

--- src/main/java/agh/ics/oop/MapDirection.java
+++ src/main/java/agh/ics/oop/MapDirection.java
@@ -3,19 +3,21 @@ package agh.ics.oop;
 import javax.lang.model.type.NullType;
 
 enum MapDirection {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST;
+    NORTH (new Vector2d(1,0), "Północ"),
+    SOUTH (new Vector2d(1,0), "Połubie"),
+    WEST (new Vector2d(1,0), "Zachód"),
+    EAST (new Vector2d(1,0), "Wschód");
+
+    private final Vector2d unitVector;
+    private final String directionName;
+
+    private MapDirection(Vector2d unitVector, String directionName){
+        this.directionName = directionName;
+        this.unitVector = unitVector;
+    }
 
     public String toString(){
-        switch (this){
-            case NORTH: return "Północ";
-            case SOUTH: return "Południe";
-            case EAST: return "Wschód";
-            case WEST: return "Zachód";
-        }
-        return null;
+        return directionName;
     }
 
     public MapDirection next(){
@@ -39,12 +41,6 @@ enum MapDirection {
     }
 
     public Vector2d toUnitVector(){
-        switch (this){
-            case NORTH: return new Vector2d(0, 1);
-            case SOUTH: return new Vector2d(0, -1);
-            case EAST: return new Vector2d(1, 0);
-            case WEST: return new Vector2d(-1, 0);
-        }
-        return null;
+        return unitVector;
     }
 }
